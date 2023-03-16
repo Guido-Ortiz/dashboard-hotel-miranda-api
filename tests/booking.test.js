96caef6d3223763380006acb2f44500fe8cf874c
Guido Ortiz <guidoortiz93@hotmail.com>
2023-03-16 15:39:18 +0100

    routes tests

--- tests/booking.test.js
+++ tests/booking.test.js
@@ -1,11 +1,37 @@
 'use strict';
-// var should = require('should');
-var app = require('../app');
-var request = require('supertest')(app);
+const request = require('supertest');
+const app = require('../app');
+jest.useFakeTimers();
 
+let token;
 
-describe('Sample Test', () => {
-    it('should test that true === true', () => {
-      expect(true).toBe(true)
+beforeAll((done) => {
+  request(app)
+    .post('/login')
+    .send({
+      email: 'admin@hotelmiranda.com',
+      password: '12345'
     })
-  })
+    .end((err, res) => {
+      token = res.body.token;
+      done();
+    });
+});
+
+describe('/bookings', () => {
+
+  it('should return 200 if authorize', () => {
+    request(app)
+      .get('/bookings')
+      .set('Authorization', 'Bearer ' + token)
+      .expect(200)
+      .expect('Content-Type', /json/)
+  });
+
+  it('should return 401 if no token was provided', () => {
+    request(app)
+      .get('/bookings')
+      .expect(401);
+  });
+
+}); 
