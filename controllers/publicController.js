e21a08bade8543d8c2d50f114c7dd61354cedebf
Guido Ortiz <guidoortiz93@hotmail.com>
2023-05-18 15:25:41 +0200

    config aws

--- controllers/publicController.js
+++ controllers/publicController.js
@@ -1,5 +1,5 @@
 exports.public_message = (req, res, next) => {
-    res.json({
+    res.status(200).json({
         name: 'Hotel Miranda',
         private_routes: [{name: '/bookings', methods: ['GET', 'POST', 'DELETE', 'PUT']}, '/rooms', '/contacts', '/users']
     })
