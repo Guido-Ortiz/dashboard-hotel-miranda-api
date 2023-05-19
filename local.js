131c4fcf451e240c71e9596558a7bf56ac1a5e9c
Guido Ortiz <guidoortiz93@hotmail.com>
2023-05-19 11:42:21 +0200

    local port

--- local.js
+++ local.js
@@ -1,4 +1,7 @@
 const app = require('./server');
 
 // run the server locally
-app.listen(3000, () => console.log('Server listening at http://localhost:3000'));
+app.listen(
+    3000, 
+    () => console.log('Server listening at http://localhost:3000')
+);
--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(session({
 app.use(passport.initialize())
 app.use(passport.session())
 
-// ENABLE ALL CORS REQUESTS
+// Enable CORS requests
 app.use(cors())
 
 app.use('/', indexRouter)
