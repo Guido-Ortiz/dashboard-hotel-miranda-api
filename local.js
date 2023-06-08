469a35b10d895737167414bcfa4f7f0e527f2c3e
Guido Ortiz <guidoortiz93@hotmail.com>
2023-06-08 09:18:16 +0200

    local server

--- local.js
+++ local.js
@@ -1,7 +1,4 @@
 const app = require('./server');
 
 // run the server locally at port number 3000
-app.listen(
-    3000, 
-    () => console.log('Server listening at http://localhost:3000')
-);
+app.listen(3000, () => console.log('Server listening at http://localhost:3000'));
